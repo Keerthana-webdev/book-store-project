e54070d54e2b2c5965a17b494c629068950b8a0d
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-04-11 14:44:39 +0530

    committed changes

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,9 @@ import {PORT , mongoDBURL} from "./config.js";
 import mongoose from 'mongoose';
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from 'cors';
 
- const app = express();
+const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
@@ -13,6 +14,23 @@ app.use(express.json());
     console.log(request);
     return response.status(234).send('Welcome To MERN Stack Tutorial');
  });
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
 
 app.use('/books' , booksRoute);
 
